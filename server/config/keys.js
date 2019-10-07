0c8c5007766fdbf61971145e89988108dbb2aeff
Pasanteweb <Pasanteweb@adm.urbe.edu>
2019-10-07 09:01:25 -0400

    nueva bd

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,8 +1,10 @@
 module.exports= {
     database:{
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'servipan'
+        host: 'us-cdbr-iron-east-05.cleardb.net',
+        user: 'b8bc05ccf42c27',
+        password: '20d18747',
+        database: 'heroku_0af4ec390c83b04'
     }
 };
+
+//mysql://b8bc05ccf42c27:20d18747@us-cdbr-iron-east-05.cleardb.net/heroku_0af4ec390c83b04?reconnect=true
