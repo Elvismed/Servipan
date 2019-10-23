44b27c291be30a3d664d5b3dbd0e62865dc367ec
Pasanteweb <Pasanteweb@adm.urbe.edu>
2019-10-23 15:56:18 -0400

    paypal errores succes

--- server/routes/paypal.js
+++ server/routes/paypal.js
@@ -13,7 +13,7 @@ paypal.configure({
   });
 const app = express();
 app.use(express.static('public'))
-app.set('views',__dirname+'/views');
+app.set('views',__dirname+'../public');
 app.engine('ejs',ejs.renderFile);
 app.set('view engine','ejs');
 // parse application/x-www-form-urlencoded
