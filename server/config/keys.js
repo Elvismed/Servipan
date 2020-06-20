c1b5d72910bce5c59b3637d7c25b1cd600fbe0e4
elvismed <elvisgpotato@gmail.com>
2020-06-20 10:01:48 -0400

    lo que falta

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,8 +1,8 @@
 module.exports= {
     database:{
-        host: 'us-cdbr-iron-east-05.cleardb.net',
-        user: 'b8bc05ccf42c27',
-        password: '20d18747',
+        host: 'localhost',
+        user: 'root',
+        password: '',
         database: 'heroku_0af4ec390c83b04'
     }
 };
--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const morgan = require("morgan");
 require('./config/config');
 //middlewares
+app.use(cors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}));
 app.use(morgan("dev"));
 app.use(express.json());
 
